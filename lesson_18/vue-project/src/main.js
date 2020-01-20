f64fcfcff21c3a082e2f39d0d0b22fd9470f4651
Vladimir Korolev <koroloff@hrscanner.ru>
2020-01-21 01:22:36 +0300

    Введение в компоненты [024]

--- lesson_18/vue-project/src/main.js
+++ lesson_18/vue-project/src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue' 
+
+Vue.component("my-counter", {
+  template: `
+    <div>
+      <h2>Счетчик: {{ counter }}</h2>
+      <button @click="add">+1</button>
+    </div>
+  `,
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    add: function() {
+      this.counter++
+    }
+  }
+}) 
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  // render: h => h(App)
 })
