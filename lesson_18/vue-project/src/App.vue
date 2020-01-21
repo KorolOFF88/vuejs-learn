91c02fadfc2f8e4849302283d78daaec3422a5e6
Vladimir Korolev <koroloff@hrscanner.ru>
2020-01-22 01:58:27 +0300

    Создание пользовательской директивы [035]

--- lesson_18/vue-project/src/App.vue
+++ lesson_18/vue-project/src/App.vue
@@ -1,30 +1,19 @@
 <template>
   <div id="app">
-    <app-pizza>
-      <h1 slot="title">{{ pizzaName }}</h1>
-      <p slot="text">Lorem, ipsum dolor.</p>
-      <h2 slot="title">Test!</h2>
-    </app-pizza>
+    <!-- Используем созданную директиву -->
+    <h2 v-colored>{{ title }}</h2>
   </div>
 </template>
 
 <script>
-import Pizza from './Pizza.vue'
-
 export default {
   data() {
     return {
-      pizzaName: "Маргаритта"
+      title: "Изучение директив"
     }
-  },
-  components: {
-    appPizza: Pizza,
   }
 }
 </script>
 
 <style scoped>
-h1 {
-  color: red
-}
 </style>
--- lesson_18/vue-project/src/main.js
+++ lesson_18/vue-project/src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue' 
+import MyDirective from './color.js'
+
+// регистрируем директиву
+Vue.directive('colored', MyDirective)
 
 new Vue({
   el: '#app',
