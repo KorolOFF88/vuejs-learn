8ae9f387dc276fd97640f94595de03737ea48374
Vladimir Korolev <koroloff@hrscanner.ru>
2020-01-21 15:29:58 +0300

    Компоненты и vue-файлы [027-1] (Глобальная регистрация)

---
+++ lesson_18/vue-project/src/Pizza.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <div>
+        <h1>Pizza name: {{ pizzaName }}</h1>
+        <p>Pizza price: {{ pizzaPrice }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                pizzaName: "Деревенская",
+                pizzaPrice: 320
+            }
+        }
+    }
+</script>
--- lesson_18/vue-project/src/main.js
+++ lesson_18/vue-project/src/main.js
@@ -1,69 +1,10 @@
 import Vue from 'vue'
-// import App from './App.vue' 
+import App from './App.vue' 
+import Pizza from './Pizza.vue'
 
-/**
- * Глобальная регистрация компонента
- * (компонент будет доступен в любом экземпляре Vue-приложения)
- */
-Vue.component("my-counter", {
-  template: `
-    <div>
-      <h2>Счетчик: {{ counter }}</h2>
-      <button @click="add">+1</button>
-    </div>
-  `,
-  data: function() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    add: function() {
-      this.counter++
-    }
-  }
-}) 
+Vue.component("app-pizza", Pizza)
 
 new Vue({
   el: '#app',
-  // render: h => h(App)
-})
-
-
-/**
- * Локальная реализация компонента
- * (компонент будет доступен только в тех Vue-приложениях, в которых он зарегистрирован)
- */
-
-// Определение локального компонента
-var localComponent2 = {
-  template: `
-    <div>
-      <h2>Счетчик: {{ counter }}</h2>
-      <button @click="add">+1</button>
-    </div>
-  `,
-  data: function() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    add: function() {
-      this.counter++
-    }
-  }
-}
-
-// Определение нового Vue-приложения
-new Vue({
-  el: '#app2',
-  // render: h => h(App)
-  components: {
-    // 'local-component' - имя, под которым будет доступен компонент
-    // также, имя компонента можно писать не в формате строки, 
-    // а в виде camplCase, к примеру:
-    // localComponent: localComponent2
-    'local-component': localComponent2
-  }
+  render: h => h(App)
 })
