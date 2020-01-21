e548fbb22e1bff639ab3f5839bacd51297a01968
Vladimir Korolev <koroloff@hrscanner.ru>
2020-01-21 23:57:48 +0300

    Валидация входящих параметров [029]

--- lesson_18/vue-project/src/Pizza.vue
+++ lesson_18/vue-project/src/Pizza.vue
@@ -1,15 +1,42 @@
 
 <template>
     <div>
-        <h1>Pizza name: {{ pizzaNameParam }}</h1>
+        <h1>Pizza name: {{ pizzaNameParam }} / {{ reverseName }}</h1>
         <p>Pizza price: {{ pizzaPriceParam }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        // Список входных параметров компонента
-        // (передаются из родительского компонента)
-        props: ['pizzaNameParam', 'pizzaPriceParam']
+        /* Определение входных параметров компонента */
+        
+        // Способ №1: Список параметров без валидации
+        // props: ['pizzaNameParam', 'pizzaPriceParam'],
+
+        // Способ №2: Параметры в виде объекта, где
+        // ключ - название параметра, значение - тип параметра
+        // props: {
+        //     pizzaNameParam: String,
+        //     pizzaPriceParam: Number
+        // },
+
+        // Способ №3: Более подробная валидация входных параметров
+        props: {
+            pizzaNameParam: {
+                type: String,
+                // required: true, // Поле обязательное
+                default: "Default pizza name" // Default значение, если параметр не передается
+            },
+            pizzaPriceParam: Number
+        },
+
+
+
+        // Вычисляемые поля, которые используются в компоненте
+        computed: {
+            reverseName() {
+                return this.pizzaNameParam.split('').reverse().join('')
+            }
+        }
     }
 </script>
